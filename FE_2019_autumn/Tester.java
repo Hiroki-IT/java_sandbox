b7f87b73af1dfab9dc86c091c77f9f3f41edf664
Hiroki <hasegawafeedshop@gmail.com>
2019-12-18 03:25:05 +0900

    過去問の実装を写経しながら、Javaの文法を学習した

---
+++ FE_2019_autumn/Tester.java
@@ -0,0 +1,39 @@
+/**
+ * テストクラス
+ */
+public class Tester
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        // エラーが発生した場合、throwsステートメントの例外を投げる
+        createUserMobileDevice("太郎", "スマホ");
+        createUserMobileDevice("太郎", "タブレット");
+        Notifier notifier = Notifier.getInstance();
+        notifier.send("太郎", "メッセージがあります。");
+        Thread.sleep(500L);
+        notifier.shutdown();
+
+        /* @ */
+    }
+    
+    /**
+     * 
+     * @param user
+     * @param name
+     */
+    private static void createUserMobileDevice(String user, String name)
+    {
+        MobileDevice device = new MobileDevice(
+            name, messageList -> System.out.println(name + ":" + messageList) // ラムダ式でのメソッド定義（引数->処理内容）
+            );
+        
+        Notifier notifier = Notifier.getInstance();
+        
+        notifier.register(user, device);
+
+        new Thread(()->{ //ラムダ式でのメソッド定義（引数なし->処理内容）
+            notifier.loopForMesseages(device);
+                System.out.printf("システム終了：%sの%s%n", user, name);
+            }).start();
+    }
+}
